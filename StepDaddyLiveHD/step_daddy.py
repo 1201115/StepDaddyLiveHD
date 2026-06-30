d05e12486149e16dc1c1d8636e95a9224c24df70
Miguel Almeida <miguel.leca.de.oliveira.almeida@rabobank.nl>
2026-06-30 10:35:06 +0100

    Update base URL to use environment variable for flexibility

--- StepDaddyLiveHD/step_daddy.py
+++ StepDaddyLiveHD/step_daddy.py
@@ -23,7 +23,7 @@ class StepDaddy:
             self._session = AsyncSession(proxy="socks5://" + socks5)
         else:
             self._session = AsyncSession()
-        self._base_url = "https://dlhd.st"
+        self._base_url = os.getenv("DLHD_BASE_URL", "https://dlhd.st")
         self.channels = []
         with open("StepDaddyLiveHD/meta.json", "r") as f:
             self._meta = json.load(f)
